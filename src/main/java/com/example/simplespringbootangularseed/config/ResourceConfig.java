3c0abc4dbcc95b581c07f25dab1d128074eb4cb9
vaibhav <vaibhav80225@gmail.com>
2017-07-18 19:32:48 +0530

    simple spring boot app with angular

---
+++ src/main/java/com/example/simplespringbootangularseed/config/ResourceConfig.java
@@ -0,0 +1,17 @@
+package com.example.simplespringbootangularseed.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class ResourceConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/webapp/**").addResourceLocations("file:webapp/");
+        super.addResourceHandlers(registry);
+    }
+    
+
+}
